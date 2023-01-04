1d89712e337322a897afdfe13509935c75e8faea
matheusjesse <matheus_jesse@hotmail.com>
2023-01-04 12:51:21 -0300

    req 06 existword

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return_data = []
+    occurrency = []
+    path_name = ""
+
+    for item in instance._data:
+        for index, row in enumerate(item["linhas_do_arquivo"], start=1):
+            if word.lower() in row.lower():
+                occurrency.append({"linha": index})
+                path_name = item["nome_do_arquivo"]
+
+    return_data.append({
+        "palavra": word,
+        "arquivo": path_name,
+        "ocorrencias": occurrency
+    })
+
+    if not len(occurrency):
+        return []
+    
+    return return_data
 
 
 def search_by_word(word, instance):
