113cd344cdd1481cea7a67b3330e41bc401937df
matheusjesse <matheus_jesse@hotmail.com>
2023-01-04 10:06:05 -0300

    req 03 file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,34 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in str(instance._data):
+        return None
+
+    data = txt_importer(path_file)
+
+    data_return = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+
+    instance.enqueue(data_return)
+    print(data_return)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos")
+
+    data = instance.dequeue()
+
+    print(f"Arquivo {data['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position))
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
